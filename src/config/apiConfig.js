72f80a14b99bba0fa78c4f605d19afaa8defa53f
PietroTy <pietro.turcimm@gmail.com>
2026-03-29 20:07:31 -0300

    Atualizando BACKEND_URL para https://counted-shadows-corrections-genius.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,7 +6,7 @@
 
 export const API_CONFIG = {
     // URL do backend - atualizada automaticamente por script
-    BACKEND_URL: 'https://maintain-texas-beauty-const.trycloudflare.com',
+    BACKEND_URL: 'https://counted-shadows-corrections-genius.trycloudflare.com',
     
     // Endpoints
     ENDPOINTS: {
