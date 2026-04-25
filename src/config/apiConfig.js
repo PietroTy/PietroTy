3170cd181a582b37e1e4e3ee900fd606b6a6bf7c
PietroTy <pietro.turcimm@gmail.com>
2026-04-24 22:44:39 -0300

    Atualizando BACKEND_URL para https://frog-impacts-gpl-bob.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,7 +6,7 @@
 
 export const API_CONFIG = {
     // URL do backend - atualizada automaticamente por script
-    BACKEND_URL: 'https://stranger-annually-city-circus.trycloudflare.com',
+    BACKEND_URL: 'https://frog-impacts-gpl-bob.trycloudflare.com',
     
     // Endpoints
     ENDPOINTS: {
