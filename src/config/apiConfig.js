7db07670370bc64ded26841e33c3f66763850bdb
PietroTy <pietro.turcimm@gmail.com>
2026-05-14 21:45:39 -0300

    Atualizando BACKEND_URL para https://area-baptist-eva-adsl.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,7 +6,7 @@
 
 export const API_CONFIG = {
     // URL do backend - atualizada automaticamente por script
-    BACKEND_URL: 'https://specified-view-meal-democrats.trycloudflare.com',
+    BACKEND_URL: 'https://area-baptist-eva-adsl.trycloudflare.com',
     
     // Endpoints
     ENDPOINTS: {
