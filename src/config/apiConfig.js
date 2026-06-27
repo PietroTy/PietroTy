c63a1f40f1c4b3aa66f783bf77dfe4e46ce47082
PietroTy <pietro.turcimm@gmail.com>
2026-06-27 14:49:10 -0300

    Atualizando BACKEND_URL para https://backgrounds-atlanta-occurring-burning.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://turns-facilities-spice-rom.trycloudflare.com',
+    BACKEND_URL: 'https://backgrounds-atlanta-occurring-burning.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
