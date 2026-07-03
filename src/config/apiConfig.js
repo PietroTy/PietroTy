a6388eacbead777b5f7938f53902a99b6f5aa8c1
PietroTy <pietro.turcimm@gmail.com>
2026-07-03 18:08:36 -0300

    Atualizando BACKEND_URL para https://enlargement-nat-prisoners-hampshire.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://carlos-meet-dancing-subjective.trycloudflare.com',
+    BACKEND_URL: 'https://enlargement-nat-prisoners-hampshire.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
