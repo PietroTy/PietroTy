caa27fa9b6b524992211586a63f031f458f61caf
PietroTy <pietro.turcimm@gmail.com>
2026-07-03 18:18:09 -0300

    Atualizando BACKEND_URL para https://injection-transition-outlined-sensors.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://enlargement-nat-prisoners-hampshire.trycloudflare.com',
+    BACKEND_URL: 'https://injection-transition-outlined-sensors.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
