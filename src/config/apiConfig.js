4f6847fcc6e5ef65dab0c802d552cae13a26cca3
PietroTy <pietro.turcimm@gmail.com>
2026-07-03 16:57:17 -0300

    Atualizando BACKEND_URL para https://blues-found-bracelet-grab.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://invision-soup-gap-device.trycloudflare.com',
+    BACKEND_URL: 'https://blues-found-bracelet-grab.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
