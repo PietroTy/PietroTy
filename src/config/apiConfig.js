521657e4c3b693cd8ff0e96aa923df5dd741849e
PietroTy <pietro.turcimm@gmail.com>
2026-07-03 17:00:19 -0300

    Atualizando BACKEND_URL para https://laugh-traffic-specialties-trademark.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://blues-found-bracelet-grab.trycloudflare.com',
+    BACKEND_URL: 'https://laugh-traffic-specialties-trademark.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
