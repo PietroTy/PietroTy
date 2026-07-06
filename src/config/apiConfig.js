7b77a5f6d0a9f3786873407f0c28ee9da30736a9
PietroTy <pietro.turcimm@gmail.com>
2026-07-06 02:14:14 -0300

    Atualizando BACKEND_URL para https://culture-joshua-agree-clan.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://deposits-switches-temple-mins.trycloudflare.com',
+    BACKEND_URL: 'https://culture-joshua-agree-clan.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
