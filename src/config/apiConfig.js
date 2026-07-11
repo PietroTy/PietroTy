2c058a88f3b439f391ec163aa2ca98e7ee732a94
PietroTy <pietro.turcimm@gmail.com>
2026-07-11 12:17:10 -0300

    Atualizando BACKEND_URL para https://switches-rest-flooring-beam.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://meetings-believed-yeast-distributed.trycloudflare.com',
+    BACKEND_URL: 'https://switches-rest-flooring-beam.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
