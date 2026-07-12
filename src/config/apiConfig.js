a792c72362244d28150fdebc77c336d629d1bd75
PietroTy <pietro.turcimm@gmail.com>
2026-07-12 12:51:51 -0300

    Atualizando BACKEND_URL para https://automotive-allied-beautifully-mission.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://switches-rest-flooring-beam.trycloudflare.com',
+    BACKEND_URL: 'https://automotive-allied-beautifully-mission.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
