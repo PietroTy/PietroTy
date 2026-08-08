16c7fa5567609b8973c9f8ad3a42778eb7360f7d
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 17:05:03 -0300

    Atualizando BACKEND_URL para https://slowly-specialties-because-craps.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://opinion-springs-tablets-using.trycloudflare.com',
+    BACKEND_URL: 'https://slowly-specialties-because-craps.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
