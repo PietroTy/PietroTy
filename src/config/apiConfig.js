6d2937d4589da6454a0288525bfd7167c4a3b9c2
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 15:39:13 -0300

    Atualizando BACKEND_URL para https://alleged-jewellery-ent-mold.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://armstrong-const-handles-extraordinary.trycloudflare.com',
+    BACKEND_URL: 'https://alleged-jewellery-ent-mold.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
