7b00f262890e398328635d029ecc6f9b3fb91f83
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 15:31:05 -0300

    Atualizando BACKEND_URL para https://armstrong-const-handles-extraordinary.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://link-irc-think-stations.trycloudflare.com',
+    BACKEND_URL: 'https://armstrong-const-handles-extraordinary.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
