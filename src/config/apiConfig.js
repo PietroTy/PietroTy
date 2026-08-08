118064d2c4689f3c410ddd0eb3838460927d52fa
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 16:44:03 -0300

    Atualizando BACKEND_URL para https://rosa-grateful-betting-melissa.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://value-width-myth-skilled.trycloudflare.com',
+    BACKEND_URL: 'https://rosa-grateful-betting-melissa.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
