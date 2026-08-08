b89926561dd1136029cef6b325f541df8a4c5fe4
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 16:39:27 -0300

    Atualizando BACKEND_URL para https://value-liberal-symbol-den.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://yarn-dans-updates-cow.trycloudflare.com',
+    BACKEND_URL: 'https://value-liberal-symbol-den.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
