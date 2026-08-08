632c447adbc6d8e7efaab5a69194303453e569c4
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 16:36:53 -0300

    Atualizando BACKEND_URL para https://yarn-dans-updates-cow.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://ppm-inserted-housewives-tax.trycloudflare.com',
+    BACKEND_URL: 'https://yarn-dans-updates-cow.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
