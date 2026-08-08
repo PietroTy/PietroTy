5f62eb811526481f7f153857bdc13fd8ab1ee004
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 17:39:04 -0300

    Atualizando BACKEND_URL para https://hats-coordinate-translations-dame.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://released-remarkable-costumes-platinum.trycloudflare.com',
+    BACKEND_URL: 'https://hats-coordinate-translations-dame.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
