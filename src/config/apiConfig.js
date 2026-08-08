16129c1c0432c157291e56684e9d56a34baca67d
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 16:20:29 -0300

    Atualizando BACKEND_URL para https://gourmet-checkout-renewal-reach.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://leasing-biography-collectables-differences.trycloudflare.com',
+    BACKEND_URL: 'https://gourmet-checkout-renewal-reach.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
