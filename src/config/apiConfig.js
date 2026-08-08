069f0953b8db2a37975b36af1020f8f6386fdc43
PietroTy <pietro.turcimm@gmail.com>
2026-08-08 15:42:03 -0300

    Atualizando BACKEND_URL para https://glen-ross-ridge-throws.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://alleged-jewellery-ent-mold.trycloudflare.com',
+    BACKEND_URL: 'https://glen-ross-ridge-throws.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
