aa1923fba269cd47210563f8ee86b563e5b900a1
PietroTy <pietro.turcimm@gmail.com>
2026-08-09 21:04:13 -0300

    Atualizando BACKEND_URL para https://peers-adaptation-baths-suites.trycloudflare.com

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BACKEND_URL: 'https://edgar-casey-letters-acc.trycloudflare.com',
+    BACKEND_URL: 'https://peers-adaptation-baths-suites.trycloudflare.com',
     ENDPOINTS: {
         chat: '/chat',
         health: '/health',
